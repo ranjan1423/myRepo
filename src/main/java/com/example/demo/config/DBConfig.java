a132906f74c37a83c98400e6a828214d02750ca6
ranjan atChetu <ranjank@chetu.com>
2020-11-17 13:13:37 +0530

    cofigured db

--- src/main/java/com/example/demo/config/DBConfig.java
+++ src/main/java/com/example/demo/config/DBConfig.java
@@ -2,4 +2,8 @@ package com.example.demo.config;
 
 public class DBconfig {
 
+    // configure databse for spring
+    private String connection_url;
+    private String userName;
+    private String passcode;
 }
