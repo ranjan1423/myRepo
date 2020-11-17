a228f04a8a7e33980bdabe33779e28aae979aadf
ranjan <ranjan1423@gmail.com>
2020-11-17 13:16:17 +0530

    modified some clases

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// main application of spring boot
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- src/main/java/com/example/demo/config/DBConfig.java
+++ src/main/java/com/example/demo/config/DBConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
 public class DBconfig {
-
+    // configure databse here
+    private String url;
+    private String user;
+    private String password;
 }
